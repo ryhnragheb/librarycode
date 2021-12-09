e5106c7349abe08ad99da1eaa3565b1efa0db891
reyhaneh <95412559+ryhnragheb@users.noreply.github.com>
2021-12-09 13:27:43 +0330

    Update book.sql

--- book.sql
+++ book.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 09, 2021 at 10:55 AM
+-- Generation Time: Dec 09, 2021 at 10:56 AM
 -- Server version: 10.4.22-MariaDB
 -- PHP Version: 7.3.33
 
@@ -42,6 +42,83 @@ CREATE TABLE `book` (
 
 INSERT INTO `book` (`bID`, `book_name`, `shabak`, `price`, `sub`, `count`) VALUES
 (0, '67', '12', '3', '45', 8);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `borrow`
+--
+
+CREATE TABLE `borrow` (
+  `borrowID` int(11) NOT NULL,
+  `mID` int(11) NOT NULL,
+  `bID` int(11) NOT NULL,
+  `sdate` date NOT NULL,
+  `fdate` date NOT NULL,
+  `situ` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `borrow`
+--
+
+INSERT INTO `borrow` (`borrowID`, `mID`, `bID`, `sdate`, `fdate`, `situ`) VALUES
+(1, 2, 1, '0000-00-00', '0000-00-00', 0),
+(3, 1, 2, '0000-00-00', '0000-00-00', 0);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `members`
+--
+
+CREATE TABLE `members` (
+  `mID` int(11) NOT NULL,
+  `name` varchar(30) NOT NULL,
+  `lname` varchar(30) NOT NULL,
+  `ncode` varchar(12) NOT NULL,
+  `phone` varchar(15) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `members`
+--
+
+INSERT INTO `members` (`mID`, `name`, `lname`, `ncode`, `phone`) VALUES
+(1, 'reyhaneh', 'ragheb', '0025', '0930'),
+(2, 'sara', 'karimi', '0015', '0912');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `borrow`
+--
+ALTER TABLE `borrow`
+  ADD PRIMARY KEY (`borrowID`);
+
+--
+-- Indexes for table `members`
+--
+ALTER TABLE `members`
+  ADD PRIMARY KEY (`mID`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `borrow`
+--
+ALTER TABLE `borrow`
+  MODIFY `borrowID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT for table `members`
+--
+ALTER TABLE `members`
+  MODIFY `mID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
